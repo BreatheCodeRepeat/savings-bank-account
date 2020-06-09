a87294c0d844309be022582cbf425aa77ce6af1a
AlexAvram <alexavram97@gmail.com>
2020-06-09 11:04:00 +0300

    Initial Commit

---
+++ src/test/java/com/banking/api/savings/SavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.api.savings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
