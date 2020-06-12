55057286bf6502f4aea78acc673d440d034a8375
AlexAvram <alexavram97@gmail.com>
2020-06-12 17:03:24 +0300

    Added documentation and removed unused imports

--- src/main/java/com/banking/api/savings/controllers/SavingsController.java
+++ src/main/java/com/banking/api/savings/controllers/SavingsController.java
@@ -3,7 +3,6 @@ package com.banking.api.savings.controllers;
 import com.banking.api.savings.dto.DepositWithdrawDTO;
 import com.banking.api.savings.models.User;
 import com.banking.api.savings.models.enums.MessageEnums;
-import com.banking.api.savings.models.enums.SavingsAccountMessageEnum;
 import com.banking.api.savings.models.enums.UserMessage;
 import com.banking.api.savings.services.SavingsService;
 import com.banking.api.savings.services.UserService;
